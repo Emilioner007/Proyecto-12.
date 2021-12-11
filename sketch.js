dfcc81e480325bcbffd8daa55756b4fdad30b0e3
Emilioner007 <92697212+Emilioner007@users.noreply.github.com>
2021-12-11 10:12:23 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -56,13 +56,13 @@ function draw() {
   //   }
   // }
 
-   if (frameCount % 80 == 0) {
+   //if (frameCount % 80 == 0) {
   //   if (select_sprites == 1) {
-       createApples();
+      // createApples();
   //   } else if (select_sprites == 2) {
   //     createOrange();
   //   }
-   }
+   //}
 
   // if (frameCount / 80 == 0) {
   //   if (select_sprites == 1) {
@@ -75,13 +75,13 @@ function draw() {
   // }
 
    if (frameCount % 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
+     if (select_sprites == 1) {
+       createApples();
+     } else if (select_sprites == 2) {
        createOrange();
-  //   }else {
-  //     createRed();
-  //   }
+     }else {
+       createRed();
+     }
    }
 
 
